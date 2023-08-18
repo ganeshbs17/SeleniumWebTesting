69470befa1c358e905b65ea43a4a034e1d7b2230
Ganesh Savant <65601315+ganeshbs17@users.noreply.github.com>
2023-08-18 19:25:03 +0530

    Create fbLogin.java

---
+++ Practice/fbLogin.java
@@ -0,0 +1,22 @@
+package homepage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class login {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("WebDriver.chrome.driver","D://Sofrwares/chromedriver_win32/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.id("email")).sendKeys("9876543210");
+		driver.findElement(By.id("pass")).sendKeys("abc123xyz");
+		driver.findElement(By.name("login")).click();
+	}
+
+}
